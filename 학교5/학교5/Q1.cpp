b50f1455bd1f97c4c57cd7871d88146943ad0637
anr4876 <dustjdanr@naver.com>
2022-04-08 23:22:58 +0900

    floyd

--- 학교5/학교5/Q1.cpp
+++ 학교5/학교5/Q1.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
-#define N 6
+#define N 30
 
-#define K 2
+#define K 10
 
 int num1 = N;
 int num2 = K;
 
 int BinomialCoefficient1(int num1, int num2) {
 
-	if (num1 == 0 || num1 == num2) {
+	if (num2 == 0 || num2 == num1) {
+		
 		return 1;
 	}
 	else {
+		
+	
 		return BinomialCoefficient1(num1 - 1, num2 - 1) + BinomialCoefficient1(num1 - 1, num2);
 	}
 }
 
 int  BinomialCoefficient2(int num1, int num2) { // 전부 다 다시 만들기
 
-	int num[N+1][K+1];
+	int num[N + 1][K + 1];
 	int i, j;
 	for(i = 0; i <= num1; i++) {
 		
-		for (j = 0; j <= i; j++) {
+		for (j = 0; j <= i && j <= num2; j++) {
 			if (j == 0 || j == i) {
 				num[i][j] = 1;
 			}
@@ -32,7 +35,8 @@ int  BinomialCoefficient2(int num1, int num2) { // 전부 다 다시 만들기
 				num[i][j] = num[i - 1][j - 1] + num[i - 1][j];
 				
 			}
-			cout << num[i][j] << "\t";
+			
+			
 		}
 		printf("\n");
 	}
@@ -48,18 +52,20 @@ void main() {
 	clock_t start, end;
 
 
-	//start = clock();
-	//cout << BinomialCoefficient1(num1, num2)<< endl;
-	//end = clock();
+	start = clock();
+	cout << BinomialCoefficient1(num1, num2)<< endl;
+	end = clock();
 
-	//double ClockNum = (double)(end - start);
-	//printf("%f", ClockNum);
+	double ClockNum = (double)(end - start);
+	printf("%f", ClockNum);
+
+	cout << endl;
 
 	start = clock();
 	cout << BinomialCoefficient2(num1, num2) << endl;
 	end = clock();
 
-	double ClockNum = (double)(end - start);
+	ClockNum = (double)(end - start);
 
 	printf("%f", ClockNum);
 }
---
+++ 학교5/학교5/Q2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int w[][5] = {
+	{0, 1, 1000, 1, 5},
+	{9, 0, 3, 2, 1000},
+	{1000, 1000, 0, 4, 1000},
+	{1000, 1000, 2, 0, 3},
+	{3, 1000, 1000, 1000, 0} 
+};
+int d[5][5], p[5][5];
+
+void floyd(int n, int w[][5], int d[][5], int p[][5]){
+	int i, j, k;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			p[i][j] = 0;
+			d[i][j] = w[i][j];
+		}
+	}
+	
+
+	for (k = 0; k < j; k++)
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+				if (d[i][k] + d[k][j] < d[i][j]) {
+					p[i][j] = k + 1;
+					d[i][j] = d[i][k] + d[k][j];
+				}
+	
+}
+
+void path(int q, int r)
+{
+	if (p[q][r] != 0) {
+		path(q, p[q][r] - 1);
+		cout << "v" << p[q][r] << " ";
+		path(p[q][r] - 1, r);
+	}
+}
+void main() {
+	int i, j, n = 5, start, goal;
+	floyd(n, w, d, p);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			cout << d[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			cout << p[i][j] << " ";
+		cout << endl;
+	}
+	cout << "출발점 : ";
+	cin >> start;
+	cout << endl << "도착점 :";
+	cin >> goal;
+	cout << "거리 = " << d[start - 1][goal - 1] << endl;
+	cout << "path = " << "v" << start << " ";
+	path(start - 1, goal - 1);
+	cout << "v" << goal << endl;
+}
